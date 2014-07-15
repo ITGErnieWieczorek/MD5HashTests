2ff6760ccc5b01e25725b4c77a65c34cfa1a8353
Ernie Wieczorek <ernest.wieczorek@intertrustgroup.com>
2014-07-15 15:44:31 -0400

    import

---
+++ MD5Hash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ITG.Utilities
+{
+    public static class MD5
+    {
+        public static string GetHash(System.IO.Stream stream)
+        {
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            return String.Concat(md5.ComputeHash(stream).Select(b => b.ToString("X2")).ToArray());
+        }
+
+        public static string GetHashV2(System.IO.Stream stream)
+        {
+                // Use input string to calculate MD5 hash
+                System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+                byte[] hashBytes  = md5.ComputeHash (stream);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                     sb.Append (hashBytes[i].ToString ("X2"));
+                     // To force the hex string to lower-case letters instead of
+                     // upper-case, use the following line instead:
+                     // sb.Append(hashBytes[i].ToString("x2")); 
+                }
+                return sb.ToString();
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            using (var fs = new FileStream("TestDocument.pdf", FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                // Calculate hash using method 1
+                var hash = ITG.Utilities.MD5.GetHash(fs);
+
+                // Reset the stream to the start of the file
+                fs.Position = 0;
+
+                // Calculate hash using method 2
+                var hashV2 = ITG.Utilities.MD5.GetHashV2(fs);
+
+                // Assert method 1 calculated the correct hash
+                Assert.That(hash.Equals("277e8c8b4417b64ffd65a4e769fee246", StringComparison.InvariantCultureIgnoreCase));
+
+                // Assert method 2 calculated the correct hash
+                Assert.That(hashV2.Equals("277e8c8b4417b64ffd65a4e769fee246", StringComparison.InvariantCultureIgnoreCase));
+
+                // Assert outputs of method 1 and method 2 are equal
+                Assert.That(hash.Equals(hashV2, StringComparison.InvariantCultureIgnoreCase));
+            }
+        }
+    }
+}
